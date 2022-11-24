c8a4bd7c6dfc8ae7845484bae8020561b2564776
MicoNoNico <56491842+MicoNoNico@users.noreply.github.com>
2022-11-24 19:23:44 +0100

    artpack and demoman

--- src/pages/projects/artpack.vue
+++ src/pages/projects/artpack.vue
@@ -1,3 +1,51 @@
 <template>
-  Stonks
+  <main>
+    <h1 class="title is-1 text-amber-700 pt-10">
+      Medieval Asset Pack
+    </h1>
+    <div class="is-size-5 p-20">
+      <br>
+      <h2 class="subtitle is-4 text-light-900">
+        A low-poly asset pack created to learn 3D modeling in Blender.
+        <br>
+        <br>
+        <h3 class="subtitle is-4 text-left text-light-900 centered">
+          Contents:
+        </h3>
+        <ul class="text-left centered is-hidden-mobile">
+          <li>19 Models</li>
+          <li>4 VFX</li>
+          <li>6 Textures/Materials</li>
+          <li>4 UI elements</li>
+        </ul>
+        <ul class="text-left is-hidden-tablet">
+          <li>19 Models</li>
+          <li>4 VFX</li>
+          <li>6 Textures/Materials</li>
+          <li>4 UI elements</li>
+        </ul>
+      </h2>
+    </div>
+    <div class="px-40">
+      <figure class="image is-16by9">
+        <iframe class="has-ratio" width="560" height="315" src="https://www.youtube.com/embed/e3lT3nWPWjI" title="YouTube video player" allowfullscreen />
+      </figure>
+    </div>
+
+    <h1 class="title is-2 text-violet-500 pt-20 centered">
+      DETAILS
+    </h1>
+    <br>
+    <h2 class="subtitle is-4 text-light-900 centered">
+      Tool: Blender
+      <br>
+      Year: 2021
+    </h2>
+  </main>
 </template>
+
+<route lang="yaml">
+meta:
+  layout: home
+  </route>
+
--- src/pages/projects/demoman.vue
+++ src/pages/projects/demoman.vue
@@ -1,3 +1,52 @@
 <template>
-  Stonks
+  <main>
+    <h1 class="title is-1 text-amber-700 pt-10">
+      Demolition Man
+    </h1>
+    <div class="is-size-5 p-20">
+      In Demolition Man, players attempt to make their way through a randomly generated city.
+      While racing against the camera, a variety of tools and gadgets can be picked up to destroy every generated building.
+      The efficacy of the tool depends on which type of block players may try to destroy. After reaching the end of a level,
+      the player is given the option to spend their score to improve certain statistics like speed or power.
+    </div>
+    <div class="px-40">
+      <figure class="image is-16by9">
+        <iframe class="has-ratio" width="560" height="315" src="https://www.youtube.com/embed/pWW7oa37l2U" title="YouTube video player" allowfullscreen />
+      </figure>
+    </div>
+
+    <div class="columns p-10">
+      <div class="column text-left">
+        <h1 class="title is-2 text-cyan-500 centered">
+          DETAILS
+        </h1>
+        <br>
+        <h2 class="subtitle is-4 text-light-900 centered">
+          Genre: Platformer
+          <br>
+          Engine: Unity
+          <br>
+          Year: 2020
+        </h2>
+      </div>
+      <div class="column text-left">
+        <h1 class="title is-2 text-cyan-500 centered">
+          ROLE
+        </h1>
+        <br>
+        <h3 class="subtitle is-5 text-light-900 centered">
+          <ul>
+            <li>Ideation & Game Design</li>
+            <li>UI Design</li>
+            <li>Level-up system programming</li>
+          </ul>
+        </h3>
+      </div>
+    </div>
+  </main>
 </template>
+
+<route lang="yaml">
+meta:
+  layout: home
+  </route>
